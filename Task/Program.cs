8a3a2291cd270ed67d9b2124eed4677d103bfe03
Alexey <pwz-and_10@mail.ru>
2022-09-19 21:41:38 +0600

    create program content

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,1 +1,26 @@
-﻿
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        void mass()
+        {
+            string[] a;
+            Console.WriteLine("Введите строки через пробел:");
+            string line = Console.ReadLine();
+            a = line.Split(' ');
+            var result = new string[a.Length];
+            var size = 0;
+            foreach (var value in a)
+            {
+                if (value.Length <= 3)
+                {
+                    result[size] = value;
+                    size++;
+                }
+            }
+            Console.WriteLine(string.Join(Environment.NewLine, result, 0, size));
+            Console.ReadKey(true);
+        }
+        mass();
+    }
+}
